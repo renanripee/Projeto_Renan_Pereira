87ca3740e0e40b3bf11200a2f7c7bd558bfe4eba
Renan Ribeiro <renan.pereira1@estudante.ufla.br>
2023-10-02 22:25:22 -0300

    Adicionando o arquivo D

---
+++ D.java
@@ -0,0 +1,12 @@
+public class D {
+    private int d1;
+    private int d2;
+
+    public void MD1(){
+        System.out.println(""+d1);
+    }
+
+    public void MD2(){
+        System.out.println(""+d2);
+    }
+}
