40e902a2f1e4d90fdc1df78f5ae1e8b2a465bedc
Renan Ribeiro <110239835+renanripee@users.noreply.github.com>
2023-10-02 21:29:42 -0300

    Adicionando os arquivos A e B

---
+++ B.java
@@ -0,0 +1,30 @@
+/* Renan Ribeiro Pereira - matricula 202210083 */
+
+public class B{
+    private int b1;
+    private float b2;
+
+    public int getB1(){
+        return b1;
+    }
+
+    public float getB2(){
+        return b2;
+    }
+
+    public void setB1(int b1){
+        this.b1 = b1;
+    }
+
+    public void setB2(float b2){
+        this.b2 = b2;
+    }
+
+    public void MB1(){
+        System.out.println("MB1");
+    }
+
+    public void MB2(){
+        System.out.println("MB2");
+    }
+}
